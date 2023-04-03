f170a6d426b4e52308f3c84b539f1b6b932475b3
Ogbonna Ogochukwu <ogochukwuzita86@gmail.com>
2023-04-03 11:08:55 +0100

    changed html structure,style and script

---
+++ index.js
@@ -0,0 +1,7 @@
+let addBtn = document.getElementById("addBtn");
+let newTsk = document.getElementById("newTsk");
+let listInput = document.getElementById("line");
+let ul = document.getElementById("line")
+addBtn.addEventListener("click", function addTask(){
+newTsk
+})
