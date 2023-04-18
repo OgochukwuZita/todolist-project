7078464ddcfca2c8b0e85932db50c876677461e2
Ogbonna Ogochukwu <ogochukwuzita86@gmail.com>
2023-04-18 09:05:34 +0100

    updated js file

--- index.js
+++ index.js
@@ -1,26 +1,49 @@
 
 
-let Todo = [];
+const Todo = [];
 
 const addBtn = document.getElementById("addBtn");
 const textField = document.getElementById("text-input-field");
-const editBtn = document.getElementById("editBtn")
-const todo = document.getElementsByClassName("todos-container")
+const editBtn = document.getElementById("editBtn");
+const todo = document.getElementById("todos-container");
+
+
 
 addBtn.addEventListener("click",()=>{
     if (textField.value.trim() === "") {
-        return(
-            alert("Please add a task!")
-        );
+    alert("Please add a task!");
+       
     }
 
 })
-addBtn.addEventListener("click",()=>{
+/*addBtn.addEventListener("click",()=>{
     return(
-    textField.value = "");
-    
-})
+    textField.value);
+})*/
 
+addBtn.addEventListener("click",function(){
+    Todo.unshift(textField.value);
+    textField.value=""
+    renderTodo();
+}
+)
 
+function renderTodo(){
+    todo.innerHTML="";
+    Todo.forEach(function(n,i){
+    todo.innerHTML +="<li>"+n+"<a onclick='editItem("+i+")'>Edit</a> <a onclick='delteItem("+i+")'>&times;</a></li>"
+    }
+    );
+}
+
+function delteItem(i){
+    Todo.splice(i,1);
+    renderTodo();
+}
+function editItem(i){
+    const newTask = prompt('Enter new task');
+    Todo.splice(i,1,newTask);
+    renderTodo();
+}
 
 
